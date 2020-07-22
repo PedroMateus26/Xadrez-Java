b0e79e2626fac233d0503c7fa56d5f567e6933b7
PedroMateus26 <pedromateusneto@gmail.com>
2020-07-22 13:49:22 -0300

    Criação das classes Board e Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,27 @@
+package boardgame;
+
+public class Piece {
+	
+	private Position position;
+	private Board board;
+	
+	public Piece(Position position, Board board) {
+		this.position = null;
+		this.board = board;
+	}
+
+	public Position getPosition() {
+		return position;
+	}
+
+	public void setPosition(Position position) {
+		this.position = position;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+
+	
+	
+}

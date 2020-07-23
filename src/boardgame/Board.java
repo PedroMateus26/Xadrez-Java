c145bc8de1e97bb1ddc127d9664044f009990ae4
PedroMateus26 <pedromateusneto@gmail.com>
2020-07-23 12:30:33 -0300

    Colocando pecas no tabuleiro

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -9,6 +9,7 @@ public class Board {
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
+		
 	}
 
 	public int getRows() {
@@ -33,5 +34,9 @@ public class Board {
 	
 	public Piece piece(Position position) {
 		return pieces[position.getRow()][position.getColumn()];
+	}
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()]=piece;
+		piece.position=position;
 	}
 }

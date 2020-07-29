d4bb6028fc303a338c9d891a22d04005b98ebd32
PedroMateus26 <pedromateusneto@gmail.com>
2020-07-29 09:40:08 -0300

    Implementando a logica de captura de pecas

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,10 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import boardgame.Board;
+
 import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.King;
@@ -10,6 +14,9 @@ public class ChessMatch {
 	private int turn;
 	private Color currentPlayer;
 	private Board board;
+	
+	private List<Piece> capturedPieces=new ArrayList<>();
+	private List<Piece> piecesOnTheBoard = new ArrayList<>();
 
 	public ChessMatch() {
 		board = new Board(8,8);
@@ -56,6 +63,10 @@ public class ChessMatch {
 		Piece p = board.removePiece(source);
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
+		if(capturedPiece!=null) {
+			piecesOnTheBoard.remove(capturedPiece);
+			capturedPieces.add(capturedPiece);
+		}
 		return capturedPiece;
 	}
 	
@@ -80,6 +91,7 @@ public class ChessMatch {
 	
 	public void placeNewPiece( char column,int row,ChessPiece piece ) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+		piecesOnTheBoard.add(piece);
 	}
 	public void initialSetup() {
 		placeNewPiece('c', 1, new Rook(board, Color.BRANCO));
